f9869c10d362fe5554e3611fb83cfdca9c4d7f76
patrigarcia <gonzalezgpatricia@hotmail.com>
2023-06-01 10:38:41 +0200

    Prueba Ramp-Up

---
+++ Ejercicio1/prueba.js
@@ -0,0 +1,23 @@
+/*
+
+Diseña un algoritmo que cuente las veces que aparece una determinada letra en una frase:
+-----------------------------------------------------------------------------------------
+
+1) Pido al usuario que ingrese una frase.
+
+2) Le pido al usuario que ingrese la letra a contar en la frase.
+
+3) Declaro un contador en 0 (variable) para que cuente las veces que aparece la letra.
+
+4) Vuelvo todas las letras de la frase a minúsculas.
+
+5) Recorro la frase letra por letra.
+
+6) Comparo cada letra de la lista con la letra que se quiere contar, si coinciden entonces incremento el contador en 1.
+
+7) Cuando termina de recorrer la frase, se muestra por pantalla un mensaje con el resultado del contador, que será
+
+la cantidad de veces que aparece la letra en la frase.
+
+Fin.
+*/
---
+++ Ejercicio2/prueba_2.js
@@ -0,0 +1,22 @@
+/*
+
+Diseña un algoritmo que imprima los números impares entre un número dado por el usuario y los siguientes 50 números
+-------------------------------------------------------------------------------------------------------------------
+
+1) Solicita al usuario que ingrese un número.
+
+2) Declaro un array vacío para almacenar los impares.
+
+3) Realizo un bucle desde el número que ingresó el usuario hasta obtener los siguientes 50 números.
+
+4) En cada iteración del bucle, pregunto si el número actual es impar.
+
+5) Si el número es impar, lo agrego al array.
+
+6) Si es par, incremento el número actual en 1.
+
+7) Cuando sale del bucle se muestra por pantalla el array con los números impares.
+
+Fin
+
+*/
